b1ea51018718e97f5c8b773c04dd299aca010c23
joshwaller-oe <josh.waller@orbiseducation.com>
2024-12-27 14:52:50 -0500

    adds demo output for additional fields

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,8 @@ const App = () => (
 	<div>
 		<Header />
 		<FormInput inputType="text" placeholderText="hello" />
+		<FormInput inputType="textarea" placeholderText="hello" />
+		<FormInput inputType="password" />
 		<AlertPopup message="also hello" alertType="success" />
 		<AlertPopup message="also hello, again" alertType="error" />
 	</div>
