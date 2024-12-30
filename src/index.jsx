d14c5e64453f7a50481bfd0e0a778fd32681d00f
Michael Fisher <75332472+mrfisher-oe@users.noreply.github.com>
2024-12-30 17:52:53 -0500

    updated code to run locally

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { FormInput, Header, AlertPopup } from "../lib";
+// * https://stackoverflow.com/questions/66384368/how-is-it-possible-to-access-homepage-from-package-json-in-a-react-app -- 12/17/2021 MF
+// * Using Vite requires a different syntax. -- 09/22/2023 MF
+import { version, copyrightYear } from '../package.json';
+const applicationVersion = version;
+
+// const componentName = "index";
+
+// console.log(componentName, "applicationVersion", applicationVersion);
+// console.log(componentName, "copyrightYear", copyrightYear);
 
 const root = createRoot(document.getElementById("root"));
 
@@ -13,7 +22,7 @@ const App = () => (
 		<AlertPopup message="also hello" alertType="success" />
 		<AlertPopup message="also hello, again" alertType="error" />
 	</div>
-)
+);
 
-root.render(<App />);
+root.render(<App applicationVersion={applicationVersion} copyrightYear={copyrightYear} />);
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// * https://vitejs.dev/config/ -- 04/04/2023 JH
+
+// * Vite with ESLint: https://www.robinwieruch.de/vite-eslint/ -- 01/23/2024 KH
+// * vite-plugin-eslint: https://github.com/gxmari007/vite-plugin-eslint -- 01/23/2024 KH
+
+// * https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js -- 09/22/2023 MF
+dotenv.config();
+
+// * The port 5173 is the Vite default port. -- 09/28/2023 MF
+// * If the port is already being used, Vite will automatically try the next available port so this may not be the actual port the server ends up listening on. (https://vitejs.dev/config/preview-options.html#preview-port) -- 09/28/2023 MF
+let applicationPort = 5173;
+
+if (process.env.PORT !== null && process.env.PORT !== undefined && process.env.PORT !== "") {
+
+	applicationPort = process.env.PORT;
+
+};
+
 export default defineConfig({
+	base: "./",
 	plugins: [react()],
+	server: {
+		port: applicationPort
+	},
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'lib/index.js'), // Entry point of your library
