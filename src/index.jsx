78bebbcb7291d846b873a10baaaf1e9c796124e7
joshwaller-oe <josh.waller@orbiseducation.com>
2025-01-10 13:39:27 -0500

    removes application from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Application, FormInput, Header, AlertPopup } from "../lib";
+import { FormInput, Header, AlertPopup } from "../lib";
 // * https://stackoverflow.com/questions/66384368/how-is-it-possible-to-access-homepage-from-package-json-in-a-react-app -- 12/17/2021 MF
 // * Using Vite requires a different syntax. -- 09/22/2023 MF
 import { version, copyrightYear } from '../package.json';
@@ -14,7 +14,7 @@ const applicationVersion = version;
 const root = createRoot(document.getElementById("root"));
 
 const App = () => (
-	<Application>
+	<div>
 		<Header applicationName="Shared Components" />
 		<FormInput inputType="text" placeholderText="hello" />
 		<FormInput inputType="textarea" placeholderText="hello" />
@@ -22,7 +22,7 @@ const App = () => (
 		<AlertPopup message="also hello" alertType="success" />
 		<AlertPopup message="also hello, again" alertType="error" />
 		<footer className="copy">&copy; {copyrightYear} Orbis Education. All rights reserved. Version: {applicationVersion}</footer>
-	</Application>
+	</div>
 );
 
 root.render(<App />);
