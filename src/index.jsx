af26cc5d066e0660a345a2a03a0074e69bc96874
Michael Fisher <75332472+mrfisher-oe@users.noreply.github.com>
2025-02-20 11:03:28 -0500

    code review updates

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { FormInput, Header, AlertPopup } from "../lib";
 // * https://stackoverflow.com/questions/66384368/how-is-it-possible-to-access-homepage-from-package-json-in-a-react-app -- 12/17/2021 MF
 // * Using Vite requires a different syntax. -- 09/22/2023 MF
-import { version, copyrightYear } from '../package.json';
+import { version, copyrightYear } from "../package.json";
 const applicationVersion = version;
 
 // const componentName = "index";
