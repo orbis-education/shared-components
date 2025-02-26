4e9933feb9d1f373ff155847de80fb55a8b1cd57
joshwaller-oe <josh.waller@orbiseducation.com>
2025-02-26 13:48:24 -0500

    adds testing of FormDropdown

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { FormInput, Header, AlertPopup } from "../lib";
+import { FormInput, FormDropdown, Header, AlertPopup } from "../lib";
 // * https://stackoverflow.com/questions/66384368/how-is-it-possible-to-access-homepage-from-package-json-in-a-react-app -- 12/17/2021 MF
 // * Using Vite requires a different syntax. -- 09/22/2023 MF
 import { version, copyrightYear } from "../package.json";
@@ -13,12 +13,40 @@ const applicationVersion = version;
 
 const root = createRoot(document.getElementById("root"));
 
+const ddAccountType = "Please select the <strong>Account Type</strong>."
+
+const inlineErrors = {
+	txtUsername: "",
+	txtPassword: "",
+	ddAccountType: "",
+	cbxUserPermissions: ""
+}
+
+const setDdAccountType = (value) => {
+	console.log("setDdAccountType", value);
+}
+
 const App = () => (
 	<div>
 		<Header applicationName="Shared Components" />
-		<FormInput inputType="text" placeholderText="hello" />
-		<FormInput inputType="textarea" placeholderText="hello" />
-		<FormInput inputType="password" />
+		<FormInput formInputID="text" inputType="text" placeholderText="hello" />
+		<FormInput formInputID="textarea" inputType="textarea" placeholderText="hello" />
+		<FormInput formInputID="password" inputType="password" />
+		<FormDropdown 
+			formInputID="ddAccountType" 
+			labelText="Account Type" 
+			isRequired={true} 
+			optionData={[
+				{ accountTypeID: 1, accountType: "User" }, 
+				{ accountTypeID: 2, accountType: "Admin" }
+			]} 
+			optionID="accountTypeID" 
+			optionText={[{ type: "property", text: "accountType" }]} 
+			inlineError={inlineErrors.ddAccountType} 
+			inputValue={ddAccountType} 
+			updateValue={setDdAccountType} 
+		/>
+
 		<AlertPopup message="also hello" alertType="success" />
 		<AlertPopup message="also hello, again" alertType="error" />
 		<footer className="copy">&copy; {copyrightYear} Orbis Education. All rights reserved. Version: {applicationVersion}</footer>
@@ -26,4 +54,3 @@ const App = () => (
 );
 
 root.render(<App />);
-
