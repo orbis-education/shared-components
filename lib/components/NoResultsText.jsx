591490df345590e5c5cd7b63a178dc57197d88ef
josephhofmeister-orbis <70538368+josephhofmeister-orbis@users.noreply.github.com>
2025-09-04 00:32:18 -0400

    add css to template and fix some issues

--- lib/components/NoResultsText.jsx
+++ lib/components/NoResultsText.jsx
@@ -1,7 +1,5 @@
 
 const NoResultsText = ({ children = "No search results." }) =>
-  <div className="card-container">
-    <p className="text-center no-results-message"><i className="fa fa-exclamation-triangle red-text"></i> {children}</p>
-  </div>;
+  <span className="text-center no-results-message"><i className="fa fa-exclamation-triangle red-text"></i> {children}</span>;
 
 export default NoResultsText;
