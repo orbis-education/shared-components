2ea129bd4e85c393206bcbae6959a7ab44252ae1
mrfisher-oe <michael.fisher@orbiseducation.com>
2025-11-19 12:31:39 -0500

    changed to use spaces instead of tabs

--- vite.config.js
+++ vite.config.js
@@ -7,37 +7,37 @@ import postcssNested from "postcss-nested";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig(({ mode }) => {
-	// * Load env file based on `mode` in the current working directory.
-	// * Set the third parameter to '' to load all env regardless of the
-	// * `VITE_` prefix.
-	const env = loadEnv(mode, process.cwd(), "");
-	return {
-		base: "./",
-		plugins: [react()],
-		server: {
-			port: env.PORT
-		},
-		css: {
-			postcss: {
-				plugins: [
-					postcssImport,
-					postcssNested,
-					autoprefixer,
-					postcssPresetEnv({ stage: 1 })
-				]
-			}
-		},
-		build: {
-			lib: {
-				entry: path.resolve(__dirname, "lib/index.js"), // * Entry point of your library
-				name: "shared-components", // * lib name
-				fileName: "shared-components", // * Output file name
-				formats: ["es", "cjs"] // * Output formats (ESM and CommonJS)
-			},
-			cssCodeSplit: false,
-			rollupOptions: {
-				external: ["react", "react-dom"] // * External dependencies
-			},
-		}
-	};
+  // * Load env file based on `mode` in the current working directory.
+  // * Set the third parameter to '' to load all env regardless of the
+  // * `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: "./",
+    plugins: [react()],
+    server: {
+      port: env.PORT
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcssImport,
+          postcssNested,
+          autoprefixer,
+          postcssPresetEnv({ stage: 1 })
+        ]
+      }
+    },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "lib/index.js"), // * Entry point of your library
+        name: "shared-components", // * lib name
+        fileName: "shared-components", // * Output file name
+        formats: ["es", "cjs"] // * Output formats (ESM and CommonJS)
+      },
+      cssCodeSplit: false,
+      rollupOptions: {
+        external: ["react", "react-dom"] // * External dependencies
+      },
+    }
+  };
 });
