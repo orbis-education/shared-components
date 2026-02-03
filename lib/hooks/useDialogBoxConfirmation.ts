7202732b644e0248d5d0f0a20a6932a885cb6acd
josephhofmeister-orbis <70538368+josephhofmeister-orbis@users.noreply.github.com>
2026-02-03 12:08:52 -0500

    fix dialog box types and issues

--- lib/hooks/useDialogBoxConfirmation.ts
+++ lib/hooks/useDialogBoxConfirmation.ts
@@ -10,13 +10,13 @@ type DialogBox = {
 
 export const useDialogBoxConfirmation = () => {
 
-  const [confirmationDialogBoxType, setConfirmationDialogBoxType] = useState<string | null>(null); // * dialogBoxTypes: confirmDelete, confirmDeleteCascade, deleteRemoteData, deleteLocalData, confirmRemove, confirmRemoveAll, changePagePrompt, resetCount, resetTable, logOut, closeProgram -- 10/23/2023 JH
+  const [confirmationDialogBoxType, setConfirmationDialogBoxType] = useState<string>(""); // * dialogBoxTypes: confirmDelete, confirmDeleteCascade, deleteRemoteData, deleteLocalData, confirmRemove, confirmRemoveAll, changePagePrompt, resetCount, resetTable, logOut, closeProgram -- 10/23/2023 JH
   const [confirmationDialogBoxSize, setConfirmationDialogBoxSize] = useState<string>(""); // * sm, md, lg, xl -- 07/15/2021 MF
   const [confirmationDialogBoxTitle, setConfirmationDialogBoxTitle] = useState<string>("");
   const [confirmationDialogBoxContent, setConfirmationDialogBoxContent] = useState<string>("");
 
   const [confirmationDialogBoxOpen, setConfirmationDialogBoxOpen] = useState<boolean>(false);
-  const [confirmationDialogBoxContinue, setConfirmationDialogBoxContinue] = useState<boolean | null>(false);
+  const [confirmationDialogBoxContinue, setConfirmationDialogBoxContinue] = useState<boolean | null>(null);
   const [deletingRecord, setDeletingRecord] = useState<boolean>(false);
   const [hardDeletingRecord, setHardDeletingRecord] = useState<boolean>(false);
 
@@ -44,9 +44,9 @@ export const useDialogBoxConfirmation = () => {
     setConfirmationDialogBoxSize("");
     setConfirmationDialogBoxTitle("");
     setConfirmationDialogBoxContent("");
-    setConfirmationDialogBoxType(null);
+    setConfirmationDialogBoxType("");
     setConfirmationDialogBoxOpen(false);
-    setConfirmationDialogBoxContinue(false);
+    setConfirmationDialogBoxContinue(null);
     setDeletingRecord(false);
     setHardDeletingRecord(false);
 
@@ -55,50 +55,46 @@ export const useDialogBoxConfirmation = () => {
 
   useEffect(() => {
 
+    if (confirmationDialogBoxContinue === null) return;
+
     if (deletingRecord) {
 
-      if (confirmationDialogBoxContinue === true) {
+      if (confirmationDialogBoxContinue) {
 
         setProcessTransactionValue("D");
         reset();
 
-      } else if (confirmationDialogBoxContinue !== false) {
+      } else {
 
         reset();
         setProcessTransactionValue("");
 
       }
 
-    }
+    } else if (hardDeletingRecord) {
 
-    if (hardDeletingRecord) {
-
-      if (confirmationDialogBoxContinue === true) {
+      if (confirmationDialogBoxContinue) {
 
         setProcessTransactionValue("HD");
         reset();
 
-      } else if (confirmationDialogBoxContinue === false) {
+      } else {
 
         reset();
         setProcessTransactionValue("");
 
       }
 
-    }
+    } else {
 
-    // * If the modal has nothing to do with deleting, then close the modal when confirmationDialogBoxContinue goes from null to true or false -- 02/05/2024 JH
-    // ? I feel like something should be changed in DialogBoxConfirmation for modals that don't need to confirmation -- 02/05/2024 JH
-    if (!deletingRecord && !hardDeletingRecord && confirmationDialogBoxContinue !== null) {
+      // * If the modal has nothing to do with deleting, then close the modal when confirmationDialogBoxContinue goes from null to true or false -- 02/05/2024 JH
+      // ? I feel like something should be changed in DialogBoxConfirmation for modals that don't need to confirm -- 02/05/2024 JH
 
       reset();
 
     }
 
   }, [deletingRecord, hardDeletingRecord, confirmationDialogBoxContinue]);
-
-
-  
 
 
   const confirmAction = (data: DialogBox) => {
@@ -112,9 +108,9 @@ export const useDialogBoxConfirmation = () => {
   };
 
 
-  const deleteRecord = (data: DialogBox) => {
+  const deleteRecord = (data?: DialogBox) => {
 
-    const newDialogBoxType = !isEmpty(data?.dialogBoxType) ? data.dialogBoxType : "confirmRemove";
+    const newDialogBoxType = !isEmpty(data?.dialogBoxType) ? data?.dialogBoxType : "confirmRemove";
 
     setDeletingRecord(true);
 
@@ -157,7 +153,7 @@ export const useDialogBoxConfirmation = () => {
 
     setConfirmationDialogBoxOpen(false);
 
-    setConfirmationDialogBoxType(null);
+    setConfirmationDialogBoxType("");
 
   };
 
